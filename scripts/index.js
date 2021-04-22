5cd7d29e13582a422b5d39bf2765660d5c3b0822
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-04-22 13:30:23 +0300

    feat(popup): finish popup functionality with JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 const openPopupButton = document.querySelector('.profile__button-edit');
+const closePopupButton = document.querySelector('.popup__close');
+const formAuthor = document.querySelector('.form__author');
 const popup = document.querySelector('.popup');
 
 function togglePopup() {
-  event.preventDefault(); 
-  popup.classList.toggle('popup_is-opened');
+  if (popup.classList.contains('.popup_opened')) {
+    clearPopupData();
+    popup.classList.toggle('popup_opened');
+  } else {
+    setPopupInitialData();
+    popup.classList.toggle('popup_opened');
+  }
 }
 
+function setPopupInitialData() {
+  document.querySelector('.form__item_el_name').value = document.querySelector('.profile__name').textContent;
+  document.querySelector('.form__item_el_about').value = document.querySelector('.profile__about').textContent;
+}
+
+function clearPopupData() {
+  document.querySelector('.form__item_el_name').value = ''; 
+  document.querySelector('.form__item_el_about').value = ''; 
+}
+
+function saveNewData(event) {
+  event.preventDefault();
+  document.querySelector('.profile__name').textContent = document.querySelector('.form__item_el_name').value;
+  document.querySelector('.profile__about').textContent = document.querySelector('.form__item_el_about').value;
+  togglePopup();
+}
+
+
 openPopupButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', togglePopup);
+formAuthor.addEventListener('submit', saveNewData);
