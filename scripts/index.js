302a3f2132407e4a1b9cf2ca87cbeda580120805
Yaroslav <yaroslav@Arjuna.localdomain>
2021-05-06 12:51:18 +0300

    feat(popup-img-fullsz): add popup with full size image with all its functionality

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,14 @@ const openPopupAddCard = document.querySelector('.profile__button-add');
 
 const closePopupProfileButton = document.querySelector('.popup__close_profile');
 const closePopupAddCardButton = document.querySelector('.popup__close_add-card');
+const closePopupFullSizeImageButton = document.querySelector('.fullszimg-popup__btn-close');
 
 const formEditProfile = document.querySelector('.form__entity_profile');
 const formAddCard = document.querySelector('.form__entity_add-card');
 
 const popupProfile = document.querySelector('.popup_edit-profile');
 const popupAddCard = document.querySelector('.popup_add-card');
+const popupFullSizeImage = document.querySelector('.fullszimg-popup');
 
 const name = document.querySelector('.profile__name');
 const about = document.querySelector('.profile__about');
@@ -47,21 +49,23 @@ function addCard(item) {
   const cardEntity = cardTemplate.content.querySelector('.card').cloneNode(true);
   cardEntity.querySelector('.card__picture').src = item.link;
   cardEntity.querySelector('.card__place').textContent = item.name;
-  document.querySelector('.places__list').append(cardEntity);
+  document.querySelector('.places__list').prepend(cardEntity);
 
   cardEntity.querySelector('.card__trashbin').addEventListener('click', function() { removeCard(event.target) });
   cardEntity.querySelector('.card__heart').addEventListener('click', function() {
     event.target.classList.contains('card__heart_color_black') ? dislikeCard(event.target) : likeCard(event.target);
   });
+  cardEntity.querySelector('.card__picture').addEventListener('click', function() { showFullSizeImage(event.target) });
 }
 
 function openPopup(popup) {
-  setPopupInitialData();
+  //Установка начальных значений полей формы модального окна необходимо только в случае открытия окна редактирования профиля
+  if (popup.classList.contains('popup_edit-profile')) { setPopupInitialData() };
   popup.classList.add('popup_opened');
 }
 
 function closePopup(popup) {
-  clearPopupData();
+  if (popup.classList.contains('popup_edit-profile')) { clearPopupData() };
   popup.classList.remove('popup_opened');
 }
 
@@ -107,11 +111,18 @@ function dislikeCard(target) {
   target.classList.remove('card__heart_color_black');
 }
 
+function showFullSizeImage(target) {
+  openPopup(popupFullSizeImage);
+  popupFullSizeImage.querySelector('.fullszimg-popup__image').src = target.src;
+  popupFullSizeImage.querySelector('.fullszimg-popup__title').textContent = target.closest('.card').querySelector('.card__place').textContent;
+}
+
 openPopupProfile.addEventListener('click', function() { openPopup(popupProfile) });
 openPopupAddCard.addEventListener('click', function() { openPopup(popupAddCard) });
 
 closePopupProfileButton.addEventListener('click', function() { closePopup(popupProfile) });
 closePopupAddCardButton.addEventListener('click', function() { closePopup(popupAddCard) });
+closePopupFullSizeImageButton.addEventListener('click', function() { closePopup(popupFullSizeImage) });
 
 
 formEditProfile.addEventListener('submit', saveNewData);
