f5ddaa35043bd0ba7f9464702b7792a2cf65dc3c
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-06-28 19:11:04 +0300

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf|webp)$/,
-        type: 'asset/resources'
+        type: 'asset/resource'
       },
       { 
         test: /\.css$/,
